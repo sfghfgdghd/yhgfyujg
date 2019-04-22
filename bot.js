6e3ee4da39182e172bfe45c2d780cf3499754dfd
sfghfgdghd <49767807+sfghfgdghd@users.noreply.github.com>
2019-04-22 21:13:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,8 +20,8 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "4";
-var adminprefix = '4'
+const prefix = "j";
+var adminprefix = 'j'
 /////////////////////////
 ////////////////////////
 
@@ -315,7 +315,7 @@ function play(guild, song) {
 }
 })
 
-const developers = ["496585065673916417","id"]
+const developers = ["558220897547452418","id"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
